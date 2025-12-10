b78ab2e48243c67353b2acbe232f95f08fe7715f
Ibrahim sedhom <ibrahimsedhom@gmail.com>
2025-12-10 18:00:13 +0400

    Fix: Add config files, install.py, and update hooks

---
+++ booking_schedule/booking_schedule/doctype/bs_booking/bs_booking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Azdan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("BS Booking", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ booking_schedule/booking_schedule/doctype/bs_merchant/test_bs_merchant.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Azdan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBSMerchant(FrappeTestCase):
+	pass
